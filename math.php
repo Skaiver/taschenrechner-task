217aff77517d7ecc7f584d6d60819b361fb4d680
Malte Schulze <xm3lte@gmail.com>
2023-04-11 19:31:03 +0200

    init old interview test task

---
+++ math.php
@@ -0,0 +1,36 @@
+<?php
+
+  function addition($x, $y) {
+    return $x + $y;
+  }
+
+  function subtract($x, $y) {
+    return $x - $y;
+  }
+
+  function multiply($x, $y) {
+    return $x * $y;
+  }
+
+  function divide($x, $y) {
+    return $x / $y;
+  }
+  
+  function root($x){
+      return sqrt($x);
+  }
+  
+  function square($x){
+      return $x** 2;
+  }
+  
+  function powerOf3($x){
+      return $x ** 3;
+  }
+  
+  function powerOfN($x, $n){
+      return $x ** $n;
+  }
+
+
+ ?>
